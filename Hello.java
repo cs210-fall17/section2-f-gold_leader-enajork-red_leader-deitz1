54696728abaa4714c787dbe255d431056698659c
Alan Keefer Deitz <deitz1@harrumph.cs.arizona.edu>
2017-09-11 17:59:15 -0700

    Fixes print statements

--- Hello.java
+++ Hello.java
@@ -9,22 +9,19 @@ public class Hello {
     }
 
     public static void callSign() {
-	System.out.println("Lock S-foils in attack position.");        
-        System.out.println("Stabilize your rear deflectors... Watch for enemy fighters."); 
 	// Red Leader should add a print statement that says "Lock S-foils in attack position." 
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
     }
 
     public static void goldLeader() {
-
-        // Gold Leader should add a print statement that says "Loosen up!"
-    
+	System.out.println("Loosen up!");
+        // Gold Leader should add a print statement that says "Loosen up!"    
 
     }
 
     public static void redLeader() {
- 
+ 	System.out.println("I copy, gold leader!");
         // Red Leader should add a print statement that says "I copy, gold leader!"
         
 
