ff4aca74b2504f3b4bf074cfbae6929097d0314a
Eric Michael Najork <enajork@harquebus.cs.arizona.edu>
2017-09-11 18:11:53 -0700

    update

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-
         goldLeader();
         redLeader();
         callSign();
@@ -10,7 +9,8 @@ public class Hello {
 
     public static void callSign() {
 	// Red Leader should add a print statement that says "Lock S-foils in attack position." 
-        // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
+        System.out.println("Stabilize your rear deflectors... Watch for enemy fighters.");
+	// Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
     }
 
     public static void goldLeader() {
